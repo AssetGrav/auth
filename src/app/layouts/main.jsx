7e2cb15eac0537a6793e29d4951b79c4f67550bc
Asset Tazhibayev <gravitationether2020@gmail.com>
2022-02-14 18:12:23 +0600

    transformation for farbase

---
+++ src/app/layouts/main.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import useMockData from "../utils/mockData";
+
+const Main = () => {
+    const { error, initialize, progress, status } = useMockData();
+    const handleClick = () => {
+        console.log("click");
+        initialize();
+    };
+    return (
+        <div className="container mt-5">
+            <h1> Main Page</h1>
+            <h3>Инициализация данных в Fairbase</h3>
+            <ul>
+                <li>Status: {status}</li>
+                <li>Progress: {progress}%</li>
+                {error && <li>Error: {error}</li>}
+            </ul>
+            <button className="btn btn-primary" onClick={handleClick}>
+                Инициализировать
+            </button>
+        </div>
+    );
+};
+
+export default Main;
